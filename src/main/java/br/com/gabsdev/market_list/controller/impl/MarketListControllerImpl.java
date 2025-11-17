873ba85ce1b46417c41c87632bed115ce49947e3
GabrielF-S <gabriel.fsantos97@outlook.com>
2025-11-17 19:46:32 -0300

    feat: adicionar endpoint updateItem para atualizar itens da lista

--- src/main/java/br/com/gabsdev/market_list/controller/impl/MarketListControllerImpl.java
+++ src/main/java/br/com/gabsdev/market_list/controller/impl/MarketListControllerImpl.java
@@ -5,21 +5,26 @@ import br.com.gabsdev.market_list.model.entity.Item;
 import br.com.gabsdev.market_list.model.entity.MarketList;
 import br.com.gabsdev.market_list.service.MarketlListService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "currentList")
 @AllArgsConstructor
+@CrossOrigin(value = "http://localhost:4200/")
 public class MarketListControllerImpl implements MarketListController {
 
     private final MarketlListService service;
     @Override
     public ResponseEntity<MarketList> closeMarketList(MarketList marketList) {
+        log.info("Recebido Lista: " + marketList.toString());
         return ResponseEntity.ok(service.closeMarketList(marketList));
     }
 
@@ -37,11 +42,19 @@ public class MarketListControllerImpl implements MarketListController {
 
     @Override
     public ResponseEntity<MarketList> addItemToMarketList(Item item) {
+        log.info("Recebido Item: " + item.getName());
         return ResponseEntity.ok(service.addToMarketList(item));
     }
 
     @Override
-    public ResponseEntity<Void> removeToCart(Item item) {
+    public ResponseEntity<Item> updateItem(Item item) {
+        log.info("Atualizado :: Item: " + item.getName() );
+            return ResponseEntity.ok(service.updateItem(item));
+
+    }
+
+    @Override
+    public ResponseEntity<MarketList> removeToCart(Item item) {
         return ResponseEntity.ok(service.removeItem(item));
     }
 
