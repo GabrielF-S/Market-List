c8dd084738e69ce3d2c3cf242ce00667324157f3
GabrielF-S <gabriel.fsantos97@outlook.com>
2025-12-01 21:51:36 -0300

    feat: Criado entidadepara item

--- src/main/java/br/com/gabsdev/market_list/config/SecurityFilter.java
+++ src/main/java/br/com/gabsdev/market_list/config/SecurityFilter.java
@@ -24,6 +24,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
         String authorizeHeader = request.getHeader("Authorization");
         if (Strings.isNotEmpty(authorizeHeader) && authorizeHeader.startsWith("Bearer")){
             String token = authorizeHeader.substring("Bearer ".length());
--- src/main/java/br/com/gabsdev/market_list/controller/impl/AuthControllerImpl.java
+++ src/main/java/br/com/gabsdev/market_list/controller/impl/AuthControllerImpl.java
@@ -15,6 +15,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,7 +40,6 @@ public class AuthControllerImpl implements AuthController {
 
     @Override
     public ResponseEntity<LoginResponse> login(LoginRequest request) {
-
         UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(
                 request.email(),
                 request.password()
